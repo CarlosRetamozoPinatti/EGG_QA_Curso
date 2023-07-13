8e0d3e3fc395f0fc90da2ca2ccdd1e60dfab59a5
CarlosRetamozoPinatti <chrp1990@gmail.com>
2023-07-13 20:58:14 -0300

    Terminado Trabajo Grupal

--- EggJavaOOP/Desafios03/DesafioGrupal/src/Main.java
+++ EggJavaOOP/Desafios03/DesafioGrupal/src/Main.java
@@ -2,6 +2,7 @@ import Service.MenuService;
 
 public class Main {
     public static void main(String[] args) {
+        //PUSE TODO EN UN MENU PORQUE QUEDABA FEO TODO EN EL MAIN.
         MenuService menu = new MenuService();
         menu.mostrarMenu();
     }
