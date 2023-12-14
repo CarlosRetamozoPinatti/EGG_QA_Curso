365b9c5affc9d507312e6a7671fd0d948fc4ca4c
CarlosRetamozoPinatti <chrp1990@gmail.com>
2023-12-14 12:21:55 -0300

    Cypress API Framework

--- Cypress/env/cypress-2/cypress/e2e/3-custom-tests/Complementario01-LoginTest.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Sample App Login', () => {
-    it('Loads login Page', () => {
-        cy.visit('http://uitestingplayground.com/sampleapp');
-        cy.get('input[name="UserName"]').type('TestUser');
-        cy.get('input[name="Password"]').type('pwd');
-        cy.get('#login').click();
-    });
-});
